426631a12b52be5d00569767319653d9a04017de
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2024-04-10 22:03:47 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,29 @@ import styles from "./page.module.css";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>app/page.tsx</code>
-                </p>
-                <div>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{" "}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+            <div className={styles.navbar}>
+                
+                <div className={styles.wordlogo}>
+                    AIMS ESPORTS
+                </div>
+
+                <div className={styles.menu}>
+                    <div className={styles.signup}>
+                        
+                    </div>
+                    <div className={styles.login}>
+
+                    </div>
                 </div>
             </div>
 
             <div className={styles.center}>
                 <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
+                    className={styles.charactersImage}
+                    src="/games.png"
+                    alt="game characters"
+                    width={1050}
+                    height={749}
                     priority
                 />
             </div>
