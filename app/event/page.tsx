aea49e2050bc1e607ca52516f1ff2390b58cb9b2
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2024-05-09 23:02:29 -0700

    event page update

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -2,11 +2,24 @@ import Navbar from '../navbar';
 import Footer from '../footer';
 import styles from './page.module.css';
 
-export default function event() {
+export default function EventPage() {
     return (
         <main className={styles.main}>
             <Navbar></Navbar>
-
+            <div className={styles.content}>
+                <h1 className={styles.h1}>Upcoming eSports Events</h1>
+                <div className={styles.events}>
+                    {/*hard-coded events, consider fetching this from your API*/}
+                    <div className={styles.event}>
+                        <h2 className={styles.h2}>Event 1</h2>
+                        <p className={styles.p}>Description of event 1</p>
+                    </div>
+                    <div className={styles.event}>
+                        <h2 className={styles.h2}>Event 2</h2>
+                        <p className={styles.p}>Description of event 2</p>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </main>
     );
